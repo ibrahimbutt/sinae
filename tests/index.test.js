cda3f64382f5085d6170de76799f5831725ce176
Ibrahim Butt <ibrahimbutt@me.com>
2019-02-06 14:37:21 +0000

    US5: On fail show result vs expected
    As a Maker,
    So I can check what wrong with my test,
    It should print out what was expected and what actually happened

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,4 +7,8 @@ describe('Sinae Tests', () => {
   it('runs a test and prints result', () => {
     expect(1).toEqual(1)
   })
+
+  it('shows on fail what was expected and returned', () => {
+    expect(1).toEqual(2)
+  })
 })

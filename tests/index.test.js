f8042af8a74742766d96e3c2d32305e71c41b297
Ibrahim Butt <ibrahimbutt@me.com>
2019-02-06 15:10:57 +0000

    Add strict equality matchers

--- tests/index.test.js
+++ tests/index.test.js
@@ -11,4 +11,21 @@ describe('Sinae Tests', () => {
   it('shows on fail what was expected and returned', () => {
     expect(1).toEqual(2)
   })
+
+  it('can check equality', () => {
+    expect(1).toEqual(1)
+  })
+
+  it('can check false equality', () => {
+    expect(1).toNotEqual(2)
+  })
+
+  it('can check strict equality', () => {
+    array = [1]
+    expect(array).toBe(array)
+  })
+
+  it('can check false equality', () => {
+    expect([1]).toNotBe([1])
+  })
 })

75000443b9488927910be8b410021371b3f29549
Ibrahim Butt <ibrahimbutt@me.com>
2019-02-07 13:07:23 +0000

    Refactor console outpout color logic

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const describe = require('./src/describe')
 const it = require('./src/it')
 const expect = require('./src/expect')
 
-module.exports = {
-  describe,
-  it,
-  expect
+module.exports = () => {
+  describe, it, expect
 }

76a68c2275194e3a18c3938b702feb674dd4703e
Ibrahim Butt <ibrahimbutt@me.com>
2019-02-07 09:34:04 +0000

    Refactor tests to be organised

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,36 +4,44 @@ const it = sinae.it
 const expect = sinae.expect
 
 describe('Sinae Tests', () => {
-  it('runs a test and prints result', () => {
-    expect(1).toEqual(1)
+  describe('scenarios', () => {
+    it('runs a test and prints result', () => {
+      expect(1).toEqual(1)
+    })
+
+    it('shows on fail what was expected and returned', () => {
+      expect(1).toEqual(2)
+    })
   })
 
-  it('shows on fail what was expected and returned', () => {
-    expect(1).toEqual(2)
+  describe('non-strict equality', () => {
+    it('can check equality', () => {
+      expect(1).toEqual(1)
+    })
+
+    it('can check false equality', () => {
+      expect(1).toNotEqual(2)
+    })
   })
 
-  it('can check equality', () => {
-    expect(1).toEqual(1)
+  describe('strict equality', () => {
+    it('can check strict equality', () => {
+      array = [1]
+      expect(array).toBe(array)
+    })
+
+    it('can check false strict equality', () => {
+      expect([1]).toNotBe([1])
+    })
   })
 
-  it('can check false equality', () => {
-    expect(1).toNotEqual(2)
-  })
+  describe('inclusion', () => {
+    it('can check if array contains', () => {
+      expect([3, 2]).contains(2)
+    })
 
-  it('can check strict equality', () => {
-    array = [1]
-    expect(array).toBe(array)
-  })
-
-  it('can check false equality', () => {
-    expect([1]).toNotBe([1])
-  })
-
-  it('can check if array contains', () => {
-    expect([3, 2]).contains(2)
-  })
-
-  it('can check if array does not contain', () => {
-    expect([3, 2]).doesNotContain(2)
+    it('can check if array does not contain', () => {
+      expect([3, 2]).doesNotContain(1)
+    })
   })
 })

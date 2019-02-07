98864b053ff254a03f3636b3338db466b9e3ffda
Ibrahim Butt <ibrahimbutt@me.com>
2019-02-07 12:05:40 +0000

    Add color to console output
    Green test descriptions for passing tests, red for failing.

--- src/it.js
+++ src/it.js
@@ -1,17 +1,21 @@
+const colors = require('./lib/colors')
+
 module.exports = (message, test) => {
   if (!test) {
     throw Error('it blocks require a function as a second argument')
   }
 
+  let color = colors.green
   let error = null
 
   try {
     test()
   } catch (thrownError) {
     error = thrownError
+    color = colors.red
   } finally {
-    console.group(message)
-    if (error) console.error(error)
+    console.group(color, message)
+    if (error) console.error(color, error)
     console.groupEnd()
   }
 }
